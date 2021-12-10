14b9c2ac5c7b0102ae93865004bdf04d77bf5f4d
Breno Gabriel Rodrigues da Silva <brenog.silva@telefonica.com>
2021-12-10 11:03:46 -0300

    Testando ordenação com Comparator

--- JavaCollectionsMapsSetsAndArraylist/src/br/com/alura/TestandoArrayList.java
+++ JavaCollectionsMapsSetsAndArraylist/src/br/com/alura/TestandoArrayList.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import br.com.alura.modelo.Aula;
 
@@ -18,8 +19,19 @@ public class TestandoArrayList {
         aulas.add(a2);
         aulas.add(a3);
 
+        // antes de ordenar por minutos:
         System.out.println(aulas);
         
-        Collections.sort(aulas);
+        /*dentro do comparing eu posso colocar qualquer método da classe
+         * desde que seja público, e poderia ser dentro do nosso exemplo
+         * Comparator.comparing(Aula::getTitulo) que iria ordenar por
+         * ordem alfabética, mas é meio inútil pra esse cenário pois
+         * tem só 1 atributo String, mas para outra onde vc tenha 2
+         * ou mais atributos String acho interessante.
+         */
+        aulas.sort(Comparator.comparing(Aula::getTempoEmMinutos));
+
+        // depois de ordenar:
+        System.out.println(aulas);
 	}
 }
