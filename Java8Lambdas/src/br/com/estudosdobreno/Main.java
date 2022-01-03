3f4ab5fee6ae39be1c8969bc141eb8aed6f5f69b
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-03 14:35:15 -0300

    Utilizando Method Reference e Ordenação por case sensitive explícita

--- Java8Lambdas/src/br/com/estudosdobreno/Main.java
+++ Java8Lambdas/src/br/com/estudosdobreno/Main.java
@@ -1,23 +1,30 @@
 package br.com.estudosdobreno;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class Main {
 
     public static void main(String[] args) {
 	    List<String> palavras = new ArrayList<String>();
         palavras.add("a23");
+        palavras.add("D123");
         palavras.add("e23234");
         palavras.add("b3221984");
+        palavras.add("d123");
         palavras.add("i");
 
         System.out.println(palavras);
 
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        palavras.sort(comparing(String::length));
 
-        palavras.forEach(s1-> System.out.println(s1));
+        palavras.forEach(System.out::println);
 
-        new Thread(() -> System.out.println("Executando um Runnable")).start();
+        palavras.sort(String.CASE_INSENSITIVE_ORDER);
+
+        palavras.forEach(System.out::println);
     }
 }
