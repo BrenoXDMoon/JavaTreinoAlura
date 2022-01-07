2d6043cf716ffbf1022ba9a8600d54232e3a1a1b
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-07 12:43:09 -0300

    Datas

---
+++ Java8Lambdas/src/br/com/estudosdobreno/Datas.java
@@ -0,0 +1,50 @@
+package br.com.estudosdobreno;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Datas {
+    public static void main(String[] args) {
+
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        LocalDate olimpiadasRio = LocalDate.of(2016, Month.JUNE, 5);
+
+        int anos = olimpiadasRio.getYear() - hoje.getYear();
+        System.out.println(anos);
+
+        Period periodo = Period.between(hoje, olimpiadasRio);
+        System.out.println(periodo);
+
+        System.out.println(periodo.getYears());
+        System.out.println(periodo.getMonths());
+        System.out.println(periodo.getDays());
+
+        System.out.println(hoje.minusYears(1));
+        System.out.println(hoje.minusMonths(4));
+        System.out.println(hoje.minusDays(2));
+
+        System.out.println(hoje.plusYears(1));
+        System.out.println(hoje.plusMonths(4));
+        System.out.println(hoje.plusDays(2));
+
+        LocalDate proximasOlimpiadas = olimpiadasRio.plusYears(4);
+        System.out.println(proximasOlimpiadas);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String valorFormatado = proximasOlimpiadas.format(formatador);
+        System.out.println(valorFormatado);
+
+        DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora.format(formatadorComHoras));
+
+        YearMonth anoEMes = YearMonth.of(2015, Month.JANUARY);
+
+        LocalTime intervalo = LocalTime.of(12, 30);
+        System.out.println(intervalo);
+
+    }
+}
