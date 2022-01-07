4a64d941f5917ad7240c178fcc14b56ddcf9f6aa
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-07 11:58:19 -0300

    Primeiros passos com Stream

---
+++ Java8Lambdas/src/br/com/estudosdobreno/ExemploCursos.java
@@ -0,0 +1,43 @@
+package br.com.estudosdobreno;
+
+import br.com.estudosdobreno.modelo.Curso;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExemploCursos {
+    public static void main(String[] args) {
+
+        List<Curso> cursos = new ArrayList<Curso>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
+
+        cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+        //cursos.forEach(c -> System.out.println(c.getNome()));
+
+        System.out.println("Filtrando cursos pela quantidade de alunos");
+
+        cursos.stream()
+                .filter(c-> c.getAlunos() >= 50)
+                .forEach(c -> System.out.println(c.getNome()));
+
+        System.out.println("Total de alunos dos cursos");
+
+        cursos.stream()
+                .filter(c -> c.getAlunos() > 50)
+                .map(Curso::getAlunos)
+                .forEach(System.out::println);
+
+
+        //convertendo uma stream de Cursos para stream de String
+        Stream<String> nomes = cursos.stream().map(Curso::getNome);
+
+        System.out.println(nomes.collect(Collectors.toList()));
+
+    }
+}
---
+++ Java8Lambdas/src/br/com/estudosdobreno/modelo/Curso.java
@@ -0,0 +1,28 @@
+package br.com.estudosdobreno.modelo;
+
+public class Curso {
+
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(int alunos) {
+        this.alunos = alunos;
+    }
+}
