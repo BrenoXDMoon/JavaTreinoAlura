157d01d524df9af40fa5f3bd8adc3779a7eb0890
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-13 11:32:25 -0300

    Usando o comando "mvn test"

---
+++ MavenGerenciamentoDependenciasBuildJava/src/test/java/ProdutoTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ProdutoTest {
+
+    @Test
+    public void test() {
+        fail("Erro gerado a fins de estudo");
+    }
+
+}
