2c0d3845d4142dbe2a7fe0a6ed4c6419d6bfbd10
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-13 11:46:43 -0300

    Atribuindo um nome para o Jar gerado no "mvn build"

--- MavenGerenciamentoDependenciasBuildJava/src/test/java/ProdutoTest.java
+++ MavenGerenciamentoDependenciasBuildJava/src/test/java/ProdutoTest.java
@@ -6,7 +6,8 @@ public class ProdutoTest {
 
     @Test
     public void test() {
-        fail("Erro gerado a fins de estudo");
+        //fail("Erro gerado a fins de estudo");
+        System.out.println("Sucesso!!");
     }
 
 }
