47919dd57b134408b54aef0a396806a3b0a08840
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-14 14:09:56 -0300

    Primeiro teste

---
+++ java-testes-unitarios-tdd/src/test/java/tdd/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package tdd;
+
+import br.com.alura.tdd.modelo.Calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void somaDeDoisNumeros() {
+        Calculadora calculadora = new Calculadora();
+
+        int soma = calculadora.somar(1, 4);
+
+        Assertions.assertEquals(5, soma);
+    }
+
+}
