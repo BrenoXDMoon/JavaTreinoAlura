07e2e838ab5c437661f4a041eb0815630f3357fc
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-21 11:08:16 -0300

    Primeiro Mock

---
+++ Mockito/src/test/java/HelloWorldMockito.java
@@ -0,0 +1,14 @@
+import br.com.alura.leilao.dao.LeilaoDao;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class HelloWorldMockito {
+
+    @Test
+    void helloWorld(){
+        LeilaoDao mock = Mockito.mock(LeilaoDao.class);
+
+        Assert.assertTrue(mock.buscarTodos().isEmpty());
+    }
+}
